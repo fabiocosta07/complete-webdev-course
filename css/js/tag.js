fec3607d7d14f5ad9dd3308d594965e5807a9edb
fabio <invillia.fabio.apolonio@ext.gympass.com>
2021-12-09 17:04:27 -0300

    css selectors

--- css/js/tag.js
+++ css/js/tag.js
@@ -19,11 +19,11 @@ const colors = {
 
 document.querySelectorAll('.tag').forEach(e => {
     const tagName = e.tagName.toLowerCase()
-    e.style.borderColor = '#616161'
+    e.style.borderColor = colors.get(tagName)
 
     if (!e.classList.contains('nolabel')) {
         const label = document.createElement('label')
-        label.style.backgroundColor = '#616161'
+        label.style.backgroundColor = colors.get(tagName)
         label.innerHTML = tagName
         e.insertBefore(label, e.childNodes[0])
     }
